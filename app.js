7c632083fea6acc16abd6c1fa9bd059ca3b94f10
TJBachorz <TJBachorz@gmail.com>
2020-09-28 15:37:22 -0600

    logs in and out and creates new Users

--- app.js
+++ app.js
@@ -1,35 +1,112 @@
 const $ = {
-    baseURL: "https://looksy-backend.herokuapp.com",
+    // baseURL: "https://looksy-backend.herokuapp.com",
+    userId: null,
+    baseURL: "http://localhost:3000",
+    loginForm: document.querySelector('.login-form'),
+    createUserForm: document.querySelector('.new-user-form'),
+    createUserButton: document.querySelector('.create-user-button'),
+    loginButton: document.querySelector('.login-button')
 }
 
-const createUserButton = document.querySelector('.create-user-button')
-const loginButton = document.querySelector('.login-button')
+$.createUserButton.addEventListener('click', (event) => displayCreateUserForm(event))
+$.loginButton.addEventListener('click', (event) => displayLoginForm(event))
+$.createUserForm.addEventListener('submit', createNewUser)
+$.loginForm.addEventListener('submit', createUserLogin)
 
-createUserButton.addEventListener('click', (event) => displayCreateUserForm(event))
-loginButton.addEventListener('click', (event) => displayLoginForm(event))
+function createUserLogin(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const username = formData.get("username")
+    const password = formData.get("password")
+    loginUser(username, password)
+}
+
+function logUserData(data) {
+    let {user, token} = data
+    localStorage.setItem('token', token)
+    localStorage.setItem('username', user.username)
+    $.userId = user.id
+}
+
+function loginUser(username, password) {
+    const headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    fetch(`${$.baseURL}/login`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({username, password})
+    }).then(response => response.json())
+        .then(logUserData)
+        // .then(createWelcomeMessage)
+    userSetup()
+}
+
+// function createWelcomeMessage(user) {
+
+// }
+
+function userSetup() {
+    deleteForms()
+    // createMenu()
+    fetchItems()
+}
+
+function deleteForms() {
+    const signUpButtons = document.querySelector('.forms')
+    signUpButtons.remove()
+}
+
+function createNewUser(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const username = formData.get("username")
+    const email = formData.get("email")
+    const password = formData.get("password")
+    postNewUser(username, email, password)
+    loginUser(username, password)
+}
+
+function postNewUser(username, email, password) {
+    const headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    const user = {
+        username,
+        email,
+        password
+    }
+    return fetch(`${$.baseURL}/users`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({user})
+    })
+}
 
 function displayLoginForm(event) {
-    const loginForm = document.querySelector('.login-form')
-    const createUserForm = document.querySelector('.new-user-form')
-    loginForm.classList.toggle("hidden")
-    if (!createUserForm.classList.contains("hidden")) {
-        createUserForm.classList.toggle("hidden")
+    $.loginForm.classList.toggle("hidden")
+    if (!$.createUserForm.classList.contains("hidden")) {
+        $.createUserForm.classList.toggle("hidden")
     }
 }
 
 function displayCreateUserForm(event) {
-    const loginForm = document.querySelector('.login-form')
-    const createUserForm = document.querySelector('.new-user-form')
-    createUserForm.classList.toggle("hidden")
-    if (!loginForm.classList.contains("hidden")) {
-        loginForm.classList.toggle("hidden")
+    $.createUserForm.classList.toggle("hidden")
+    if (!$.loginForm.classList.contains("hidden")) {
+        $.loginForm.classList.toggle("hidden")
     }
 }
 
-// fetchItems()
-
 function fetchItems() {
-    fetch(`${$.baseURL}/items`)
+    fetch(`${$.baseURL}/items`, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('token')}`
+        }
+    })
         .then(response => response.json())
         .then(render3DModels)
 }
@@ -80,4 +157,8 @@ function generateModelCard() {
     modelCard.classList.add('model-card')
     cardContainer.appendChild(modelCard)
     return modelCard
+}
+
+function logout() {
+    localStorage.clear()
 }
